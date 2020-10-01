5c8dddd91af8c6ef55afb88fd4d988f59c9e1f59
Ирина Вадим Cидоренко <C:\Users\aleyn\AppData\Roaming\The Bat!>
2020-10-01 18:32:09 +0300

    A-Q-H_v2

---
+++ build/precache-manifest.8993566482a355060155e8fd771da7c2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf736b44852afb6b918503b018f0d0a",
+    "url": "/Vadim74-web/air-quality/index.html"
+  },
+  {
+    "revision": "e902e7e8779916d02b15",
+    "url": "/Vadim74-web/air-quality/static/css/2.24e2894b.chunk.css"
+  },
+  {
+    "revision": "bdcdded77d0d6a7527be",
+    "url": "/Vadim74-web/air-quality/static/css/main.9681027a.chunk.css"
+  },
+  {
+    "revision": "e902e7e8779916d02b15",
+    "url": "/Vadim74-web/air-quality/static/js/2.8e6e1964.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Vadim74-web/air-quality/static/js/2.8e6e1964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdcdded77d0d6a7527be",
+    "url": "/Vadim74-web/air-quality/static/js/main.32fa4ae0.chunk.js"
+  },
+  {
+    "revision": "410535cd4fb9c927b2cc",
+    "url": "/Vadim74-web/air-quality/static/js/runtime-main.19b5e49f.js"
+  },
+  {
+    "revision": "0662d9de8af86fb66060a046fbafe1b5",
+    "url": "/Vadim74-web/air-quality/static/media/GC_logo.0662d9de.svg"
+  },
+  {
+    "revision": "3573d221b566ebfaf5f95a160435af71",
+    "url": "/Vadim74-web/air-quality/static/media/Hero-BG.3573d221.jpg"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/Vadim74-web/air-quality/static/media/ITN_logo.2f311a05.svg"
+  },
+  {
+    "revision": "1c0e7e35cd0ed8ed7e1ac3026726d97d",
+    "url": "/Vadim74-web/air-quality/static/media/Vector.1c0e7e35.svg"
+  },
+  {
+    "revision": "f867bb0e9da11199c30068cf79f84fef",
+    "url": "/Vadim74-web/air-quality/static/media/burger.f867bb0e.svg"
+  },
+  {
+    "revision": "5ec44869469e2f3c9db3551c44662f88",
+    "url": "/Vadim74-web/air-quality/static/media/sector-slide1.5ec44869.jpg"
+  },
+  {
+    "revision": "65643b05446e10934c8778999860e9de",
+    "url": "/Vadim74-web/air-quality/static/media/sector-slide2.65643b05.jpg"
+  },
+  {
+    "revision": "f8f67844654cd9fc78dd57834ecfed6c",
+    "url": "/Vadim74-web/air-quality/static/media/sector-slide3.f8f67844.jpg"
+  },
+  {
+    "revision": "fb084e1e416e340515649cb48eb04f6e",
+    "url": "/Vadim74-web/air-quality/static/media/sector-slide4.fb084e1e.jpg"
+  },
+  {
+    "revision": "43f9dac62ffb9209e823b91b7504c5ec",
+    "url": "/Vadim74-web/air-quality/static/media/sector-slide5.43f9dac6.jpg"
+  },
+  {
+    "revision": "a8b6caeec11d336276f5ed05a63fc938",
+    "url": "/Vadim74-web/air-quality/static/media/sector-slide6.a8b6caee.jpg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/Vadim74-web/air-quality/static/media/shid_emozhl_logo.341a487c.svg"
+  },
+  {
+    "revision": "f9f2551a855d5dde626eeb560bd167b6",
+    "url": "/Vadim74-web/air-quality/static/media/slider-fact1.f9f2551a.jpg"
+  },
+  {
+    "revision": "e3f084b1f2b67ae9b915639c3ac532ee",
+    "url": "/Vadim74-web/air-quality/static/media/slider-fact2.e3f084b1.jpg"
+  },
+  {
+    "revision": "ea41cbe353e0353cad1da6c54f8d6304",
+    "url": "/Vadim74-web/air-quality/static/media/slider-fact3.ea41cbe3.jpg"
+  },
+  {
+    "revision": "fed4245627a01891c733cbe6f17c11c1",
+    "url": "/Vadim74-web/air-quality/static/media/slider-fact4.fed42456.jpg"
+  },
+  {
+    "revision": "5f157730b95c254d7e94b15eed8eda96",
+    "url": "/Vadim74-web/air-quality/static/media/slider-fact5.5f157730.jpg"
+  }
+]);
